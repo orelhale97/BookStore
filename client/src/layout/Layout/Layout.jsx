acde6be331ccf7ba9c163e9d798f5911f32987da
Orel hale <orelh.ravtech.co.il>
2025-05-25 19:25:29 +0300

    Main

--- client/src/layout/Layout/Layout.jsx
+++ client/src/layout/Layout/Layout.jsx
@@ -3,31 +3,21 @@ import './Layout.scss'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
 import Sidebar from '../Sidebar/Sidebar'
-import { Navigate, Route, Router, Routes } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext'
+import { useAppContext } from '../../context/AppContext'
 import Popup from '../../components/Popup/Popup'
-import BooksPage from '../../pages/BooksPage/BooksPage'
-import PurchasesPage from '../../pages/PurchasesPage/PurchasesPage'
+
 
 
 export default function Layout() {
 
-   const { showPopup, setShowPopup } = useAuth()
+   const { showPopup, setShowPopup } = useAppContext()
 
    return (
       <div className='Layout' key={'Layout'}>
          <Header />
          <div className='app-container'>
             <Sidebar />
-
-            <div className='app-main'>
-               <Routes>
-                  <Route path="/" element={<Navigate to="/books" replace />} />
-                  <Route path="books" element={<BooksPage />} />
-                  <Route path="history" element={<PurchasesPage />} />
-                  <Route path="*" element={<h1 style={{ color: "red", textAlign: "center", marginTop: "200px" }}>Page not found</h1>} />
-               </Routes>
-            </div>
+            <Main />
          </div>
 
          {showPopup && <Popup onClose={() => setShowPopup(null)}>{showPopup}</Popup>}
--- client/src/layout/Main/Main.jsx
+++ client/src/layout/Main/Main.jsx
@@ -1,12 +1,23 @@
-import BooksPage from '../../pages/BooksPage/BooksPage'
 import './Main.scss'
 
+import BooksPage from '../../pages/BooksPage/BooksPage'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import PurchasesPage from '../../pages/PurchasesPage/PurchasesPage'
+import AuthorsPage from '../../pages/AuthorsPage/AuthorsPage'
+import PublisherPage from '../../pages/publisherPage/publisherPage'
 
 export default function Main() {
 
     return (
-        <div className='Main'>
-            <BooksPage />
+        <div className='app-main'>
+            <Routes>
+                <Route path="/" element={<Navigate to="/books" replace />} />
+                <Route path="books" element={<BooksPage />} />
+                <Route path="history" element={<PurchasesPage />} />
+                <Route path="authors" element={<AuthorsPage />} />
+                <Route path="publishers" element={<PublisherPage />} />
+                <Route path="*" element={<h1 style={{ color: "red", textAlign: "center", marginTop: "200px" }}>Page not found</h1>} />
+            </Routes>
         </div>
     )
 }
